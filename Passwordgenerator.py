b98adb4d22d4cc25f3aa231c3ce33fc6de2cfe1e
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-04-04 16:58:20 +0100

    Password generator GUI

--- Passwordgenerator.py
+++ Passwordgenerator.py
@@ -22,36 +22,39 @@ def generator(password_length: int, is_digit: bool = True, is_special: bool = Tr
 def main():
     window = tk.Tk()
     window.title("Password generator")
+    icon = tk.PhotoImage(file="password.png")
+    window.iconphoto(True, icon)
 
     heading = tk.Label(window, text="Password generator", font=("oswald", 50, "italic"),
                        fg="black", background="gold", relief="groove", pady=10)
-    heading.pack()
+    heading.pack(pady=20)
 
     length_label = tk.Label(window, text="Enter password length:", font=("oswald", 20), pady=10)
-    length_label.pack()
+    length_label.pack(pady=5)
 
     length_entry = tk.Entry(window, font=("oswald", 20), relief="raised")
-    length_entry.pack()
+    length_entry.pack(pady=2)
+    length_entry.bind('<Return>', lambda event: update_password())
 
     numeric_label = tk.Label(window, text="Do you want numbers? (leave blank if not): ", font=("oswald", 20), pady=10)
-    numeric_label.pack()
+    numeric_label.pack(pady=5)
 
     numeric_entry = tk.Entry(window, font=("oswald", 20), relief="raised")
     numeric_entry.insert(0, 'True')
-    numeric_entry.pack()
+    numeric_entry.pack(pady=2)
 
     special_label = tk.Label(window, text="Do you want special characters? (leave blank if not): ",
                              font=("oswald", 20), pady=10)
-    special_label.pack()
+    special_label.pack(pady=5)
 
 
     special_entry = tk.Entry(window, font=("oswald", 20))
     special_entry.insert(0, 'True')
-    special_entry.pack()
+    special_entry.pack(pady=2)
 
     generated = tk.Label(window, text="Click the button to generate a password",
-                         font=("oswald", 25, "italic"), relief="raised", pady=10)
-    generated.pack()
+                         font=("oswald", 25, "italic"), relief="solid", pady=10)
+    generated.pack(pady=10)
 
     def update_password():
         password_length = length_entry.get()
@@ -64,9 +67,20 @@ def main():
         password = generator(password_length, nums_check, special_check)
         generated.config(text=password)
 
+    def copy_password():
+        if generated['text'] == "Click the button to generate a password":
+            return
+        window.clipboard_clear()
+        window.clipboard_append(generated['text'])
+        generated.config(text="Password copied!")
+
     update_password_button = tk.Button(window, text="Generate password", font=("oswald", 20),
-                                       command=update_password, relief="solid", pady=20)
-    update_password_button.pack()
+                                       command=update_password, relief="raised", pady=20)
+    update_password_button.pack(pady=10)
+
+    copy_password_button = tk.Button(window, text="Copy password", font=("oswald", 20),
+                                     command=copy_password, relief="raised", pady=10)
+    copy_password_button.pack(pady=5)
 
     window.mainloop()
 
