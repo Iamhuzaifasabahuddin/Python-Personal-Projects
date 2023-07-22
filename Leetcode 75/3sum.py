3a8ab9ecf2471eb38452d2c5ebb1cddd79ab4bc7
Huzaifa Sabahuddin <67235423+erierintegral77@users.noreply.github.com>
2023-07-22 23:19:55 +0100

    Update 3sum.py

--- Leetcode 75/3sum.py
+++ Leetcode 75/3sum.py
@@ -8,11 +8,11 @@ class Solution(object):
         nums.sort()
 
         for i, value in enumerate(nums):
-            if i > 0 and value == nums[i + 1]:
+            if i > 0 and value == nums[i - 1]:
                 continue
-            left, right = 0, len(nums) - 1
+            left, right = i + 1, len(nums) - 1
             while left < right:
-                threesum = nums[left] + nums[right] + value
+                threesum = value + nums[left] + nums[right] 
                 if threesum > 0:
                     right -= 1
                 if threesum < 0:
@@ -22,4 +22,11 @@ class Solution(object):
                     left += 1
                     while left < right and left == nums[i - 1]:
                         left = left + 1
+                   # reference why do we use a loop is to move forward as 
+                   # if to decrement it the previous conditions do that for you.
+                   # We could also use 3 for loop method
+                   # for i in range(len(nums)-2): because last two wont be checked.
+                   #     for j in rane(i,len(nums)-1): last wont be checked.
+                   #         for k in range(j, len(nums)):
+
         return result
