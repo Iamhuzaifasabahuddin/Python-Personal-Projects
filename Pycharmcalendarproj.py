bb00a00f592058b2f40e3c5cbd7b89e631f86317
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-07-24 02:49:12 +0100

    Calendar project

--- Pycharmcalendarproj.py
+++ Pycharmcalendarproj.py
@@ -1,12 +1,15 @@
 import datetime
 import os.path
-import time
+import sqlite3
+from sys import argv
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from dateutil import parser  # type: ignore
+
+from google.auth.transport.requests import Request  # type: ignore
+from google.oauth2.credentials import Credentials  # type: ignore
+from google_auth_oauthlib.flow import InstalledAppFlow  # type: ignore
+from googleapiclient.discovery import build  # type: ignore
+from googleapiclient.errors import HttpError  # type: ignore
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -33,33 +36,116 @@ def main():
         # Save the credentials for the next run
         with open('token2.json', 'w') as token:
             token.write(creds.to_json())
+    while True:
+        operation = input("Enter an operation add, commit, view or exit: ")
+        if operation == 'add':
+            des = str(input("Enter description: ")).upper()
+            hr = float(input("Enter Hours: "))
+            add_event(creds, des, hr)
+        if operation == 'view':
+            num = int(input("Enter number of days to view: "))
+            getHours(num)
+        if operation == 'commit':
+            commit_hours(creds)
+        if operation == 'exit':
+            break
+        elif operation not in ['add', 'view', 'commit', 'exit']:
+            print("Enter a valid operation")
 
+
+def commit_hours(creds):
+    service = build('calendar', 'v3', credentials=creds)
+
+    today = datetime.date.today()
+    start_time = str(today) + "T00:00:00Z"
+    end_time = str(today) + "T23:59:59Z"
+
+    events_result = service.events().list(calendarId='primary', timeMin=start_time, timeMax=end_time,
+                                          maxResults=10, singleEvents=True,
+                                          orderBy='startTime', timeZone='Europe/London').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+        return
+    total_duration = datetime.timedelta(
+        seconds=0,
+        minutes=0,
+        hours=0,
+    )
+
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        end = event['end'].get('dateTime', event['end'].get('date'))
+
+        start_formatted = parser.isoparse(start)  # changing the start time to datetime format
+        end_formatted = parser.isoparse(end)  # changing the end time to datetime format
+        duration = end_formatted - start_formatted
+
+        total_duration += duration
+        print(f"{event['summary'].capitalize()}, duration: {duration}")
+    print(f"Total duration: {total_duration} Hours")
+
+    # adding the hours to the database
+    Connection = sqlite3.connect('Timetable.db')
+    cursor = Connection.cursor()
+    print("Opened database successfully")
+    date = datetime.date.today()
+
+    formatted_total_duration = total_duration.seconds / 60 / 60
+    coding_hours = (date, 'CODING', formatted_total_duration)
+    cursor.execute("INSERT INTO hours VALUES(?, ?, ?);", coding_hours)
+    Connection.commit()
+    print("Coding hours added to database successfully")
+
+
+def add_event(creds, description: str, duration: float):
     try:
-        service = build('calendar', 'v3', credentials=creds)
-
-        work_duration = datetime.timedelta(hours=1)  # Set the duration you want to work (e.g., 2 hours)
-
         start_time = datetime.datetime.utcnow()
-        end_time = start_time + work_duration
+        end_time = datetime.datetime.utcnow() + datetime.timedelta(hours=int(duration))
+        start_time_formatted = start_time.isoformat() + 'Z'
+        end_time_formatted = end_time.isoformat() + 'Z'
 
         event = {
-            'summary': 'PyCharm Event',
+            'summary': description,
             'description': 'This is an event created every time I open PyCharm.',
             'colorId': 7,
             'start': {
-                'dateTime': start_time.isoformat() + 'Z',
+                'dateTime': start_time_formatted,
                 'timeZone': 'Europe/London',  # Replace with your time zone, e.g., 'America/New_York'
             },
             'end': {
-                'dateTime': end_time.isoformat() + 'Z',
+                'dateTime': end_time_formatted,
                 'timeZone': 'Europe/London',
             },
         }
 
+        service = build('calendar', 'v3', credentials=creds)
         event = service.events().insert(calendarId='primary', body=event).execute()
         print(f'Event created: {event.get("htmlLink")}')
     except HttpError as error:
         print('An error occurred: %s' % error)
+
+
+def getHours(Number_of_days: int):
+    try:
+        today = datetime.date.today()
+        when_from = today + datetime.timedelta(days=-int(Number_of_days))
+        Connection = sqlite3.connect('Timetable.db')
+        Cursor = Connection.cursor()
+        Cursor.execute(f"SELECT DATE, HOURS FROM hours WHERE DATE between ? AND ?", (when_from, today))
+
+        hours = Cursor.fetchall()
+
+        total_hours = 0
+        for element in hours:
+            print(f"{element[0]}: {element[1]}")
+            total_hours += element[1]
+        print(f"Total hours: {total_hours}")
+        print(f"Average hours: {total_hours / float(Number_of_days):.2f}")
+
+    except Exception as e:
+        print("An error occurred", e)
 
 
 if __name__ == '__main__':
