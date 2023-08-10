5ae36b1853b51fe02f48358640d52b4a9b21dca2
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-08-10 04:52:35 +0100

    leetcode

--- Leetcode 75/Tripletsubsequence.py
+++ Leetcode 75/Tripletsubsequence.py
@@ -7,15 +7,29 @@ class Solution:
         second_min = float('inf')  # Initialize with positive infinity
 
         for num in nums:
+
             if num <= first_min:
                 first_min = num
             elif num <= second_min:
                 second_min = num
             else:
                 return True
-
         return False
 
+
+# class Solution:
+#     def increasingTriplet(self, nums: list[int]) -> bool:
+#         n = len(nums)
+#         maxRight = [0] * n  # maxRight[i] is the maximum element among nums[i+1...n-1]
+#         maxRight[-1] = nums[-1]
+#         for i in range(n - 2, -1, -1):
+#             maxRight[i] = max(maxRight[i + 1], nums[i + 1])
+#         minLeft = nums[0]
+#         for i in range(1, n - 1):
+#             if minLeft < nums[i] < maxRight[i]:
+#                 return True
+#             minLeft = min(minLeft, nums[i])
+#         return False
 
 if __name__ == '__main__':
     S = Solution()
