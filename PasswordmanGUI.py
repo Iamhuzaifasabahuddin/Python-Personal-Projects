b7e10100e46cace00c0becd7efd25aa93081402e
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-08-14 00:47:11 +0100

    Password GUI

--- PasswordmanGUI.py
+++ PasswordmanGUI.py
@@ -6,15 +6,22 @@ import tkinter as tk
 
 
 # main funcs
-def add(account, password, message_label, add_acc_entry, add_pwd_entry):
-    with open("NewPasswords.txt", 'a') as f:
-        f.write(f"Account: {account}, Password: {password}\n")
+def add(account, password, message_label, add_acc_entry, add_pwd_entry, toggle_add_fields, window):
+    if account.strip() == "" or password.strip() == "":
+        message_label.config(text="Account or Password cannot be empty", fg="red")
+        message_label.pack()  # Make sure to pack the label even if the input is empty
+    else:
+        with open("NewPasswords.txt", 'a') as f:
+            f.write(f"Account: {account}, Password: {password}\n")
 
-    add_acc_entry.delete(0, tk.END)
-    add_pwd_entry.delete(0, tk.END)
+        add_acc_entry.delete(0, tk.END)
+        add_pwd_entry.delete(0, tk.END)
 
-    message_label.config(text="Account added successfully!", fg="green")
-    message_label.pack()
+        toggle_add_fields(None)
+
+        message_label.config(text="Account added successfully!", fg="green")
+        message_label.pack()
+        window.after(2000, lambda: message_label.pack_forget())
 
 
 def view():
@@ -41,6 +48,7 @@ def master():
     return NotImplementedError
 
 
+
 def gui():
     window = tk.Tk()
     window.title("Password manager")
@@ -59,40 +67,37 @@ def gui():
     add_pwd_label = tk.Label(window, text="Enter your password: ")
     add_acc_entry = tk.Entry(window, font=("oswald", 20))
     add_acc_entry.bind('<Return>', )
-    add_pwd_entry = tk.Entry(window, font=("oswald", 20))
-    message_label = tk.Label(window, text="", font=("oswald", 14))
+    add_pwd_entry = tk.Entry(window, font=("oswald", 20), show="*")
+    add_message_label = tk.Label(window, text="", font=("oswald", 14))
     add_button = tk.Button(window, text="Add Account", command=lambda: add(add_acc_entry.get(), add_pwd_entry.get(),
-                                                                           message_label, add_acc_entry, add_pwd_entry),
+                                                                           add_message_label, add_acc_entry,
+                                                                           add_pwd_entry,
+                                                                           toggle_add_fields, window),
                            font=("oswald", 20))
     add_acc_entry.bind("<Return>", lambda event: add(add_acc_entry.get(), add_pwd_entry.get(),
-                                                     message_label, add_acc_entry, add_pwd_entry))
+                                                     add_message_label, add_acc_entry, add_pwd_entry, toggle_add_fields,
+                                                     window))
     add_pwd_entry.bind("<Return>", lambda event: add(add_acc_entry.get(), add_pwd_entry.get(),
-                                                     message_label, add_acc_entry, add_pwd_entry))
+                                                     add_message_label, add_acc_entry, add_pwd_entry, toggle_add_fields,
+                                                     window))
 
     def toggle_add_fields(event):
-        selected_value = items.get()
-        if selected_value == "add":
-            add_acc_label.pack()
-            add_acc_entry.pack()
-            add_pwd_label.pack()
-            add_pwd_entry.pack()
-            add_button.pack()
-            message_label.pack()
-
-        else:
             add_acc_label.pack_forget()
             add_acc_entry.pack_forget()
             add_pwd_label.pack_forget()
             add_pwd_entry.pack_forget()
             add_button.pack_forget()
-            message_label.pack_forget()
 
     def get_value(event):
         """Return the value from the combobox"""
         selected_value = items.get()
 
         if selected_value == "add":
-            toggle_add_fields(event)
+            add_acc_label.pack()
+            add_acc_entry.pack()
+            add_pwd_label.pack()
+            add_pwd_entry.pack()
+            add_button.pack()
         elif selected_value == "view":
             master()
             view()
