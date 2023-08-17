46e4ee8b0b34aaf543ad01ccb2e4efd1ce4df369
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-08-17 01:13:17 +0100

    Testing for password manager GUI

--- Table2.py
+++ Table2.py
@@ -6,11 +6,11 @@ cursor = connection.cursor()
 print("Connection established")
 
 # f = connection.execute("SELECT * FROM manager")
+#
+# f = cursor.execute("DELETE FROM manager")
+f1 = cursor.execute("SELECT USERNAME, ACCOUNT, PASSWORD FROM manager WHERE USERNAME=?", ("Facebook",))
 
-f = cursor.execute("DELETE FROM manager")
-
-connection.commit()
-
-print(f.fetchall())
-
+res = f1.fetchall()
+for r in res:
+    print(r[1])
 
