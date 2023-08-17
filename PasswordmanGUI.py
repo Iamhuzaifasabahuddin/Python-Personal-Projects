04beaf5fa107a8fa3a9bad47de3765888fac8ec8
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-08-17 01:12:04 +0100

    updated functions and search

--- PasswordmanGUI.py
+++ PasswordmanGUI.py
@@ -12,7 +12,8 @@ def add(username, account, password, message_label, add_acc_entry, add_pwd_entry
     else:
         connection = sqlite3.connect('Passwords.db')
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO manager VALUES(?, ?, ?)", (selected_account_value, account, password))
+        cursor.execute("INSERT INTO manager VALUES(?, ?, ?)", (selected_account_value.title(),
+                                                               account.title(), password))
         connection.commit()
 
         add_acc_entry.delete(0, tk.END)
@@ -41,12 +42,53 @@ def upload():
     return NotImplementedError
 
 
-def exist():
-    return NotImplementedError
+def exist(username, message_label, toggle_search, window):
+    if username.strip() == "":
+        message_label.config(text="Please enter an account! ", fg="red")
+        message_label.pack()
+        window.after(2000, lambda: message_label.pack_forget())
+    else:
+        connection = sqlite3.connect("Passwords.db")
+        cursor = connection.cursor()
+        res = cursor.execute("SELECT USERNAME, ACCOUNT, PASSWORD FROM manager WHERE USERNAME=?", (username.title(),))
+        result = res.fetchall()
+        if not result:
+            message_label.config(text="Account doesnt exist", fg="red")
+            message_label.pack()
+            toggle_search(False)
+            window.after(2000, lambda: message_label.pack_forget())
+        else:
+            message_label.config(text=f"ACCOUNT EXIST \n ACCOUNT: {result[0][0]} \n USERNAME: {result[0][1]} "
+                                      f"\n PASSWORD: {result[0][2]}", fg="green")
+            message_label.pack()
+            toggle_search(False)
+            window.after(2000, lambda: message_label.pack_forget())
 
 
-def master():
-    return NotImplementedError
+def master(password, message_label, master_entry, option, toggle_master, toggle_search, window):
+    with open("master.txt", "r") as file:
+        master_password = file.readline().strip()
+    if password.strip() == "":
+        message_label.config(text="Please enter your password!", fg="red")
+        message_label.pack()
+        window.after(2000, lambda: message_label.pack_forget())
+    else:
+        if password == master_password:
+            message_label.config(text="Authorization successful", fg="green")
+            message_label.pack()
+            master_entry.delete(0, tk.END)
+            toggle_master()
+            if option == "search":
+                toggle_search(True)
+            elif option == "view":
+                raise NotImplementedError
+            window.after(2000, lambda: message_label.pack_forget())
+        else:
+            message_label.config(text="Authorization failed", fg="red")
+            message_label.pack()
+            master_entry.delete(0, tk.END)
+            toggle_search(False)
+            window.after(2000, lambda: message_label.pack_forget())
 
 
 def gui():
@@ -55,7 +97,6 @@ def gui():
     window.geometry("500x500")
 
     main_label = tk.Label(text="Welcome to the Password Manager")
-
 
     # Main taskbox for adding
     Tasks = ["Options", "add", "view", "edit", "delete", "upload", "search"]
@@ -96,10 +137,25 @@ def gui():
                                                add_pwd_entry, toggle_add_fields, window),
                            font=("oswald", 20))
 
-    def reset_taskbox():
-        Task_box.set(Tasks[0])
+    master_label = tk.Label(window, text="Enter Master Password: ")
+    master_entry = tk.Entry(window, font=("oswald", 20), show="*")
+    master_main_label = tk.Label(window, text="", font=("oswald", 14))
+    master_button = tk.Button(window, text="Check",
+                              command=lambda: master(master_entry.get(), master_main_label, master_entry, toggle_master,
+                                                     items,
+                                                     toggle_search, window), font=("oswald", 20))
 
+    search_label = tk.Label(window, text="Enter Account To Search: ")
+    search_entry = tk.Entry(window, font=("oswald", 20))
+    search_main_label = tk.Label(window, text="", font=("oswald", 14))
+    search_button = tk.Button(window, text="Search",
+                              command=lambda: exist(search_entry.get(), search_main_label, toggle_search, window))
+
+    def reset():
+        Task_box.set(Tasks[0])
+    # Used to unpack all add fields
     def toggle_add_fields():
+        """Unpacks all add fields"""
         add_acc_label.pack_forget()
         add_acc_entry.pack_forget()
         add_pwd_label.pack_forget()
@@ -107,9 +163,25 @@ def gui():
         add_user_label.pack_forget()  # Hide the label for adding a new account
         Accounts_box.pack_forget()
         add_button.pack_forget()
-        reset_taskbox()
 
-    def get_value(event):
+    def toggle_master():
+        """Unpacks all master"""
+        master_entry.pack_forget()
+        master_label.pack_forget()
+        master_button.pack_forget()
+
+    def toggle_search(enable):
+        if enable:
+            search_label.pack()
+            search_entry.pack()
+            search_button.pack()
+        else:
+            search_label.pack_forget()
+            search_entry.pack_forget()
+            search_button.pack_forget()
+
+    # Used to get the value from the main combobox
+    def get_value(event=None):
         """Return the value from the combobox"""
         selected_value = items.get()
 
@@ -130,10 +202,12 @@ def gui():
         else:
             toggle_add_fields()
             if selected_value == "view":
-                master()
                 view()
             elif selected_value == "search":
-                exist()
+                master_label.pack()
+                master_entry.pack()
+                master_button.pack()
+                toggle_search(False)
             elif selected_value == "delete":
                 delete()
             elif selected_value == "edit":
@@ -147,7 +221,7 @@ def gui():
     Task_label.pack()
     Task_box.pack()
 
-    # Bind the Return key to the entry fields and button
+    # Add Bindings
     add_acc_entry.bind("<Return>", lambda event: add(selected_account, add_acc_entry.get(), add_pwd_entry.get(),
                                                      add_message_label, add_acc_entry, add_pwd_entry,
                                                      toggle_add_fields,
@@ -156,10 +230,18 @@ def gui():
                                                      add_message_label, add_acc_entry, add_pwd_entry,
                                                      toggle_add_fields,
                                                      window))
-    add_button.bind("<Return>", lambda event: add(selected_account, add_acc_entry.get(), add_pwd_entry.get(),
-                                                  add_message_label, add_acc_entry, add_pwd_entry,
-                                                  toggle_add_fields,
-                                                  window))
+    add_button.config(command=lambda: add(selected_account, add_acc_entry.get(), add_pwd_entry.get(),
+                                          add_message_label, add_acc_entry, add_pwd_entry,
+                                          toggle_add_fields,
+                                          window))
+    # Master Bindings
+    master_entry.bind("<Return>", lambda event: master(master_entry.get(), master_main_label, master_entry,
+                                                       items.get(), toggle_master, toggle_search, window))
+    master_button.config(command=lambda: master(master_entry.get(), master_main_label, master_entry,
+                                                items.get(), toggle_master, toggle_search, window))
+    # Search binding
+    search_entry.bind("<Return>", lambda event: exist(search_entry.get(), search_main_label, toggle_search, window))
+    search_button.config(command=lambda: exist(search_entry.get(), search_main_label, toggle_search, window))
 
     window.mainloop()
 
