01fe3c0be5707b4aced9fcce2774fddda37185c8
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-08-21 05:08:18 +0100

    Updated

--- PasswordmanGUI.py
+++ PasswordmanGUI.py
@@ -1,6 +1,5 @@
-import datetime
 import tkinter as tk
-import tkinter.ttk
+from tkinter import ttk, scrolledtext
 import sqlite3
 import os
 import logging
@@ -11,6 +10,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow  # type: ignore
 from googleapiclient.discovery import build  # type: ignore
 from googleapiclient.http import MediaFileUpload  # type: ignore
 from googleapiclient.errors import HttpError  # type: ignore
+
+current_selection = None
 
 
 # main funcs
@@ -91,8 +92,8 @@ def edit(search, account, username, password, message_label, edit_search, toggle
         cursor.execute("UPDATE manager SET ACCOUNT=?, USERNAME=?, PASSWORD=? WHERE USERNAME=?",
                        (new_account.title(), new_username.title(), new_password, search))
         connection.commit()
-        show_message(message_label, "Account updated", "green")
         toggle_edit(False)
+        show_message(message_label, "Account updated", "green")
     connection.close()
 
 
@@ -117,7 +118,7 @@ def delete(Account, message_label, toggle_delete):
     connection.close()
 
 
-def upload(Passcode, message_label, upload_entry, toggle_upload):
+def upload(message_label, toggle_upload):
     """
         Upload the database file to Google Drive.
 
@@ -125,92 +126,81 @@ def upload(Passcode, message_label, upload_entry, toggle_upload):
         after validating a provided passcode.
 
         Args:
-            Passcode (str): The passcode for validation before proceeding with the upload.
             message_label (tk.Label): The label widget to display messages.
-            upload_entry (tk.Entry): The entry widget containing the passcode.
             toggle_upload (function): Function to toggle upload fields visibility.
     """
-    with open("Passcodeupload.txt", "r") as file:
-        code = file.readline().strip()
-    if Passcode.strip() == "":
-        show_message(message_label, "Passcode Field Cannot Be Empty!", "red")
+    toggle_upload(False)
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(r'C:\Users\huzai\PycharmProjects\Python-projects-1\Google\token3.json'):
+        creds = Credentials.from_authorized_user_file(
+            r'C:\Users\huzai\PycharmProjects\Python-projects-1\Google\token3.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                r'C:\Users\huzai\PycharmProjects\Python-projects-1\Google\Drive_Credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(r'C:\Users\huzai\PycharmProjects\Python-projects-1\Google\token3.json', 'w') as token:
+            token.write(creds.to_json())
 
-    if Passcode.strip() == code:
-        toggle_upload(False)
-        SCOPES = ['https://www.googleapis.com/auth/drive']
-        """Shows basic usage of the Drive v3 API.
-        Prints the names and ids of the first 10 files the user has access to.
-        """
-        creds = None
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists(r'C:\Users\huzai\PycharmProjects\Python-projects-1\Google\token3.json'):
-            creds = Credentials.from_authorized_user_file(
-                r'C:\Users\huzai\PycharmProjects\Python-projects-1\Google\token3.json', SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    r'C:\Users\huzai\PycharmProjects\Python-projects-1\Google\Drive_Credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open(r'C:\Users\huzai\PycharmProjects\Python-projects-1\Google\token3.json', 'w') as token:
-                token.write(creds.to_json())
+    try:
+        service = build('drive', 'v3', credentials=creds)
 
-        try:
-            service = build('drive', 'v3', credentials=creds)
+        response = service.files().list(
+            q="name='Hexzdrivefolder' and mimeType='application/vnd.google-apps.folder'",
+            spaces='drive').execute()
 
-            response = service.files().list(
-                q="name='Hexzdrivefolder' and mimeType='application/vnd.google-apps.folder'",
-                spaces='drive').execute()
-
-            if not response['files']:
-                file_metadata = {
-                    "name": "Hexzdrivefolder",
-                    "mimeType": "application/vnd.google-apps.folder",
-                }
-
-                file = service.files().create(body=file_metadata, fields="id").execute()
-                folder_id = file.get('id')
-            else:
-                folder_id = response['files'][0]['id']
-
+        if not response['files']:
             file_metadata = {
-                "name": "MyPasswords.db",
-                "parents": [folder_id]
+                "name": "Hexzdrivefolder",
+                "mimeType": "application/vnd.google-apps.folder",
             }
 
-            file_path = r"C:\Users\huzai\PycharmProjects\Python-projects-1\Passwords.db"
-            # Search for the existing file in Google Drive
-            existing_file = service.files().list(
-                q=f"name='MyPasswords.db' and parents='{folder_id}'",
-                spaces='drive').execute()
+            file = service.files().create(body=file_metadata, fields="id").execute()
+            folder_id = file.get('id')
+        else:
+            folder_id = response['files'][0]['id']
 
-            if existing_file['files']:
-                existing_file_id = existing_file['files'][0]['id']
+        file_metadata = {
+            "name": "MyPasswords.db",
+            "parents": [folder_id]
+        }
 
-                media = MediaFileUpload(file_path, resumable=True)
-                update_file = service.files().update(fileId=existing_file_id, media_body=media).execute()
+        file_path = r"C:\Users\huzai\PycharmProjects\Python-projects-1\Passwords.db"
+        # Search for the existing file in Google Drive
+        existing_file = service.files().list(
+            q=f"name='MyPasswords.db' and parents='{folder_id}'",
+            spaces='drive').execute()
 
-                logging.info(f"Updated file Name {update_file.get('name')}")
-                show_message(message_label, "File Updated Successfully", "green")
-            else:
-                media = MediaFileUpload(file_path)
-                upload_file = service.files().create(body=file_metadata, media_body=media).execute()
-                logging.info(f"Uploaded file Name: {upload_file.get('name')}")
-                show_message(message_label, "File Uploaded To Google Drive", "green")
-        except HttpError as error:
-            logging.info(f'An error occurred: {error}')
-            show_message(message_label, "An error occurred during upload", "red")
-    else:
-        show_message(message_label, "Invalid Passcode!", "red")
-        upload_entry.delete(0, tk.END)
+        if existing_file['files']:
+            existing_file_id = existing_file['files'][0]['id']
+
+            media = MediaFileUpload(file_path, resumable=True)
+            update_file = service.files().update(fileId=existing_file_id, media_body=media).execute()
+
+            logging.info(f"Updated file Name {update_file.get('name')}")
+            show_message(message_label, "File Updated Successfully", "green")
+        else:
+            media = MediaFileUpload(file_path)
+            upload_file = service.files().create(body=file_metadata, media_body=media).execute()
+            logging.info(f"Uploaded file Name: {upload_file.get('name')}")
+            show_message(message_label, "File Uploaded To Google Drive", "green")
+    except HttpError as error:
+        logging.info(f'An error occurred: {error}')
+        show_message(message_label, "An error occurred during upload", "red")
 
 
-def exist(username, message_label, toggle_search):
+def exist(username, message_label, toggle_search, search_listbox):
     """
         Check if an account exists in the database.
 
@@ -221,6 +211,7 @@ def exist(username, message_label, toggle_search):
             username (str): The account username to search for.
             message_label (tk.Label): The label widget to display messages.
             toggle_search (function): Function to toggle search fields visibility.
+            search_listbox(tk.ListBox): Creates a Taskbox which is also scrollable
     """
     if username.strip() == "":
         show_message(message_label, "Please enter an account! ", "red")
@@ -230,15 +221,19 @@ def exist(username, message_label, toggle_search):
         res = cursor.execute("SELECT USERNAME, ACCOUNT, PASSWORD FROM manager WHERE USERNAME=?", (username.title(),))
         result = res.fetchall()
         if not result:
+            toggle_search(False)
             show_message(message_label, "Account doesnt exist", "red")
-            toggle_search(False)
         else:
-            show_message(message_label, f"ACCOUNT EXIST \n ACCOUNT: {result[0][0]} \n USERNAME: {result[0][1]} "
-                                        f"\n PASSWORD: {result[0][2]}", "green")
+            show_message(message_label,"Showing Account / Accounts", "green")
+            for index, values in enumerate(result, start=1):
+                search_listbox.insert(tk.END, f"Account {index}\n\n")  # We insert from END due to it adds from where
+                # it left off
+                search_listbox.insert(tk.END, f"ACCOUNT: {values[0]}\nUSERNAME: {values[1]}\nPASSWORD: {values[2]}\n\n")
+            search_listbox.pack()  # Display the scrolled text widget
             toggle_search(False)
 
 
-def master(password, message_label, master_entry, option, toggle_master, toggle_search):
+def master(password, message_label, master_entry, option, toggle_master, toggle_search, toggle_upload):
     """
        Validate the master password and perform corresponding actions.
 
@@ -260,17 +255,19 @@ def master(password, message_label, master_entry, option, toggle_master, toggle_
         show_message(message_label, "Please enter your password!", "red")
     else:
         if password == master_password:
-            show_message(message_label, "Authorization successful", "green")
             master_entry.delete(0, tk.END)
             toggle_master(False)
+            show_message(message_label, "Authorization successful", "green")
             if option == "search":
                 toggle_search(True)
             elif option == "view":
-                show_message(message_label,"Unavailable", "red")
+                show_message(message_label, "Unavailable", "red")
+            elif option == "upload":
+                toggle_upload(True)
         else:
-            show_message(message_label, "Authorization failed", "red")
             master_entry.delete(0, tk.END)
             toggle_search(False)
+            show_message(message_label, "Authorization failed", "red")
 
 
 def gui():
@@ -283,7 +280,7 @@ def gui():
     """
     window = tk.Tk()
     window.title("Password manager")
-    window.geometry("500x500")
+    window.geometry("600x600")
     icon = tk.PhotoImage(file="password.png")
     window.iconphoto(True, icon)
 
@@ -300,7 +297,7 @@ def gui():
     Task_box["state"] = 'readonly'
 
     # Accounts adding ComboBox
-    Accounts = ["Facebook", "X", "Instagram", "Gmail", "LinkedIn", "Github", "Hotmail", "University", "Other"]
+    Accounts = [" ", "Facebook", "X", "Instagram", "Gmail", "LinkedIn", "Github", "Hotmail", "University", "Other"]
     add_user_label = tk.Label(window, text="Select account to add: ")
     accounts = tk.StringVar()
     Accounts_box = tk.ttk.Combobox(window, textvariable=accounts, font=("oswald", 20))
@@ -308,6 +305,7 @@ def gui():
     Accounts_box["state"] = 'readonly'
 
     def selected_account(event=None):
+        """Edits the state of the account combobox"""
         if accounts.get() == "Other":
             add_user_label.config(text="Enter account: ")
             Accounts_box.config(state='normal')  # Allow editing the combobox
@@ -340,14 +338,18 @@ def gui():
                               command=lambda: master(master_entry.get(), master_main_label, master_entry, items.get(),
                                                      toggle_master,
 
-                                                     toggle_search), font=("oswald", 20))
+                                                     toggle_search, toggle_upload), font=("oswald", 20))
+
     # Search Function Fields
     search_label = tk.Label(window, text="Enter Account To Search: ", font=("oswald", 10))
     search_entry = tk.Entry(window, font=("oswald", 20))
     search_main_label = tk.Label(window, text="", font=("oswald", 14))
+    search_listbox = scrolledtext.ScrolledText(window, wrap=tk.WORD, width=40, height=20, font=("oswald", 14))
     search_button = tk.Button(window, text="Search",
-                              command=lambda: exist(search_entry.get(), search_main_label, toggle_search),
+                              command=lambda: exist(search_entry.get(), search_main_label, toggle_search,
+                                                    search_listbox),
                               font=("oswald", 20))
+
     # Edit Function Fields
     edit_label = tk.Label(window, text="Enter Account To Edit: ", font=("oswald", 10))
     edit_search = tk.Entry(window, font=("oswald", 20))
@@ -370,13 +372,13 @@ def gui():
     delete_Button = tk.Button(window, text="Delete", command=lambda: delete(delete_entry.get(), delete_main_label,
                                                                             toggle_delete, ))
 
-    upload_label = tk.Label(window, text="Enter Upload Passcode: ", font=("oswald", 10))
-    upload_entry = tk.Entry(window, font=("oswald", 20), show="*")
+    # Upload Function Fields
+    upload_label = tk.Label(window, text="Enter to Upload: ", font=("oswald", 10))
     upload_main_label = tk.Label(window, text="", font=("oswald", 14))
-    upload_button = tk.Button(window, text="Upload", command=lambda: upload(upload_entry.get(), upload_main_label,
-                                                                            upload_entry, toggle_upload))
+    upload_button = tk.Button(window, text="Upload", command=lambda: upload(upload_main_label, toggle_upload))
 
     def reset():
+        """ Resets the main combobox to index 0."""
         Task_box.set(Tasks[0])
 
     # Used to unpack all add fields
@@ -414,6 +416,7 @@ def gui():
             master_entry.pack_forget()
             master_label.pack_forget()
             master_button.pack_forget()
+            reset()
 
     # Used to unpack all search fields
     def toggle_search(enable):
@@ -422,11 +425,14 @@ def gui():
             search_label.pack(pady=5)
             search_entry.pack(pady=5)
             search_button.pack(pady=10)
+            search_listbox.delete("1.0", tk.END)  # Clear the text in the scrolled text widget
         else:
             search_label.pack_forget()
             search_entry.pack_forget()
             search_button.pack_forget()
             search_entry.delete(0, tk.END)
+            # Use the after method to display the search_listbox after 10 seconds
+            search_listbox.after(60000, lambda: search_listbox.pack_forget())
 
     # Used to unpack all edit fields
     def toggle_edit(enable):
@@ -473,19 +479,15 @@ def gui():
         """Packs and unpacks all upload fields"""
         if enable:
             upload_label.pack(pady=5)
-            upload_entry.pack(pady=5)
             upload_button.pack(pady=10)
         else:
             upload_label.pack_forget()
-            upload_entry.pack_forget()
             upload_button.pack_forget()
-            upload_entry.delete(0, tk.END)
 
     # Used to get the value from the main combobox
     def get_value(event=None):
         """Return the value from the combobox"""
         selected_value = items.get()
-
         if selected_value == "Options":
             main_label.config(text="Please select an action!", fg="red")
             main_label.pack()  # Show the main label when "Options" is selected
@@ -495,41 +497,41 @@ def gui():
             toggle_delete(False)
             toggle_search(False)
             toggle_master(False)
-            reset()
+            search_listbox.pack_forget()
         elif selected_value == "view":
             toggle_master(True)
             toggle_search(False)
             toggle_delete(False)
             toggle_edit(False)
             toggle_add_fields(False)
+            search_listbox.pack_forget()
         elif selected_value == "search":
             toggle_master(True)
             toggle_search(False)
             toggle_delete(False)
             toggle_edit(False)
+            toggle_add_fields(False)
         elif selected_value == "delete":
             toggle_delete(True)
             toggle_search(False)
             toggle_edit(False)
             toggle_add_fields(False)
             toggle_master(False)
-            reset()
+            search_listbox.pack_forget()
         elif selected_value == "edit":
             toggle_edit(True)
             toggle_search(False)
             toggle_delete(False)
             toggle_master(False)
             toggle_add_fields(False)
-            reset()
+            search_listbox.pack_forget()
         elif selected_value == "upload":
-            toggle_upload(True)
             toggle_search(False)
             toggle_delete(False)
             toggle_edit(False)
-            toggle_master(False)
+            toggle_master(True)
             toggle_add_fields(False)
-            reset()
-
+            search_listbox.pack_forget()
         else:
             return "Select a value from the dropdown list"
 
@@ -549,7 +551,7 @@ def gui():
         edit_entry_username: edit_entry_password,
         edit_entry_password: edit_button,
         delete_entry: delete_Button,
-        upload_entry: upload_button,
+        upload_button: upload_button,
     }
 
     # Define a function to handle Enter key press
